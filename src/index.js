44adbd11c6706ac51e52ea50b4726e2e46816c2c
Md. Minul Alam <minul.alam@leads-bd.com>
2021-12-15 14:41:34 +0600

    css changed

--- src/index.js
+++ src/index.js
@@ -44,8 +44,8 @@ const kafka = new Kafka({
   sasl: {
     mechanism: "plain",
     username:
-      "ebiwocidev/bhaswati@ebiw.com/ocid1.streampool.oc1.iad.amaaaaaafcron3aalem6chmj63rd6hk52km5pxrriygf55vevnnesaqp6fwq",
-    password: "IVj4vj}9CFb19-Oh5o}k",
+      "ebiwocidev/bhaswati@ebiw.com/ocid1.streampool.oc1.iad.amaaaaaafcron3aai4sgpesswehlslv3724v677q777u4bq5kkukxnqg7yya",
+    password: "h5oGJ1DIn3[hN#zl2wsW",
   },
 })
 
@@ -64,19 +64,23 @@ const consumer = kafka.consumer({ groupId: "test-group" })
 const run = async () => {
   io.on("connection", async (client) => {
     console.log("Connected", client)
-    await consumer.connect()
-    await consumer.subscribe({ topic: "ebiw1234", fromBeginning: true })
-    await consumer.run({
-      eachMessage: async ({ topic, partition, message }) => {
-        let data = JSON.parse(message.value)
-        console.log("data: ", data)
-        await client.emit("event", data)
-      },
-    })
-    client.on("disconnect", async () => {
-      console.log("Client disconnected")
-      await consumer.disconnect()
-    })
+    try {
+      await consumer.connect()
+      await consumer.subscribe({ topic: "ebiw123", fromBeginning: true })
+      await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+          let data = JSON.parse(message.value)
+          console.log("data: ", data)
+          await client.emit("event", data)
+        },
+      })
+      client.on("disconnect", async () => {
+        console.log("Client disconnected")
+        await consumer.disconnect()
+      })
+    } catch (err) {
+      console.log(err)
+    }
   })
 }
 
